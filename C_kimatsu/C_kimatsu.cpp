9b7f513201ba498c224c00350f0e72bc3f7b8bc4
motoki0332 <motoki0332@gmail.com>
2023-07-25 21:43:00 +0900

    2023/07/25

--- C_kimatsu/C_kimatsu.cpp
+++ C_kimatsu/C_kimatsu.cpp
@@ -24,6 +24,12 @@ int main()
 	initscr();
 
 	start_color();
+	init_color(8, 1000, 753, 796);	// Pink
+	init_color(9, 1000, 647, 0);	// Orange
+	init_color(10, 500, 500, 500);	// Gray
+	init_color(11, 1000, 1000, 878);	// LightYellow
+	init_color(12, 1000, 871, 678);	// NavajoWhite
+
 	init_pair(1, COLOR_RED, COLOR_RED);				// 数字オブジェクト2のカラー
 	init_pair(2, COLOR_WHITE, COLOR_RED);
 
@@ -48,18 +54,20 @@ int main()
 	init_pair(15, COLOR_WHITE, COLOR_WHITE);		// 数字オブジェクト256のカラー
 	init_pair(16, COLOR_BLACK, COLOR_WHITE);
 
-	init_pair(17, COLOR_RED, COLOR_RED);			// 数字オブジェクト512のカラー
-	init_pair(18, COLOR_BLACK, COLOR_RED);
+	init_pair(17, 8, 8);							// 数字オブジェクト512のカラー
+	init_pair(18, COLOR_WHITE, 8);
 
-	init_pair(19, COLOR_GREEN, COLOR_GREEN);		// 数字オブジェクト1024のカラー
-	init_pair(20, COLOR_BLACK, COLOR_GREEN);
+	init_pair(19, 9, 9);							// 数字オブジェクト1024のカラー
+	init_pair(20, COLOR_WHITE, 9);
 
-	init_pair(21, COLOR_YELLOW, COLOR_YELLOW);		// 数字オブジェクト2048のカラー
-	init_pair(22, COLOR_BLUE, COLOR_YELLOW);
+	init_pair(21, 10, 10);							// 数字オブジェクト2048のカラー
+	init_pair(22, COLOR_WHITE, 10);
 
-	init_pair(23, COLOR_WHITE, COLOR_BLACK);		// フィールドのカラー
+	init_pair(23, 12, 12);							// フィールドのカラー
 
-	init_pair(24, COLOR_RED, COLOR_BLACK);			// リザルトのカラー
+	init_pair(24, COLOR_BLACK, 11);					// リザルトのカラー
+
+	init_pair(25, 12, 12);							// 背景色
 
 	noecho();
 	cbreak();
